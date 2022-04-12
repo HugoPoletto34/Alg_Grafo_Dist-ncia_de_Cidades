39aae4e4b88833f04793a944837e8f5d88d40b4f
HugoPoletto34 <hugopoletto34@gmail.com>
2022-04-12 19:57:21 -0300

    Atualizando grafo, vertice e aresta

--- src/main/java/models/Grafo.java
+++ src/main/java/models/Grafo.java
@@ -1,66 +1,59 @@
 package models;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Classe básica para um models.Grafo simples
  */
 public class Grafo {
-    public static final int MAX_VERTICES = 100;
     private static int ultimoId = 0;
     protected int qtdVertices;
 
     /**
      * Um grafo tem um vetor de vértices, que conterão as arestas
      */
-    protected Vertice[] vertices;
+    protected List<Vertice> vertices;
 
     /**
      * Construtor. Cria um grafo vazio com capacidade para MAX_VERTICES
      */
     public Grafo(){
         this.qtdVertices = 0;
-        this.vertices = new Vertice[MAX_VERTICES];
+        this.vertices = new LinkedList<>();
     }
 
     /**
      * Método de acesso ao vetor de vértices
      * @return Vetor com vértices do grafo
      */
-    public Vertice[] getVertices(){
+    public List<Vertice> getVertices(){
             return this.vertices;
+    }
+
+    public Vertice getVertice(int indice) {
+        return this.vertices.get(indice);
     }
 
     /**
      * Adiciona, se possível, um vértice ao grafo. O vértice é auto-nomeado com o próximo id disponível.
      */
     public void addVertice(){
-        if(this.qtdVertices < MAX_VERTICES){
-            Vertice novo = new Vertice(ultimoId);
-            this.vertices[qtdVertices] = novo;
-            this.qtdVertices++;
-            ultimoId++;
-        }
+        Vertice novo = new Vertice(ultimoId);
+        this.vertices.add(novo);
+        this.qtdVertices++;
+        ultimoId++;
     }
 
-    /**
-     * Adiciona uma aresta entre dois vértices do grafo. 
-     * Não verifica se os vértices pertencem ao grafo.
-     * @param origem Vértice de origem
-     * @param destino Vértice de destino
-     */
     public void addAresta(Vertice origem, Vertice destino){
         origem.addAresta(1, destino);
     }
 
-
-    /**
-     * Verifica se este é um grafo completo. 
-     * @return TRUE para grafo completo, FALSE caso contrário
-     */
     public boolean completo(){
         boolean resposta = true;
         
         for(int i=0; i<this.qtdVertices; i++){
-            if(this.vertices[i].grau()!= (this.qtdVertices-1)){
+            if(this.vertices.get(i).grau()!= (this.qtdVertices-1)){
                 resposta = false;
                 break;
             }
@@ -68,15 +61,12 @@ public class Grafo {
         return resposta;
     }
 
-    /** 
-     * Descrição do grafo em string (vértices e seus vizinhos) 
-     * */
     @Override
     public String toString(){
-        StringBuilder aux = new StringBuilder("DESCRIÇÃO DO GRAFO: "+"\n");
+        StringBuilder aux = new StringBuilder("Grafo:");
 
         for(int i=0; i<this.qtdVertices; i++){
-            aux.append(this.vertices[i].toString()+"\n");
+            aux.append(" --- " + this.vertices.get(i).toString());
         }
 
         return aux.toString();
--- src/main/java/models/Vertice.java
+++ src/main/java/models/Vertice.java
@@ -1,16 +1,13 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Vertice {
-    private static final int MAX_ARESTAS = 100;
-    private int qtArestas;    
+    private int qtArestas;
     private int id;
-
-    /**
-     * Um vértice tem um vetor de arestas para outros vértices
-     */
-    private Aresta[] arestas;
+    private List<Aresta> arestas;
 
     /**
      * Construtor para criação de vértice identificado
@@ -19,22 +16,17 @@ public class Vertice {
     public Vertice(int id){
         this.id = id;
         this.qtArestas=0;
-        this.arestas = new Aresta[MAX_ARESTAS];
+        this.arestas = new LinkedList<>();
     }
 
     public int getId() {
         return this.id;
     }
 
-    /**
-     * Verifica se já existe aresta entre este vértice e um destino. Método privado
-     * @param dest Vértice de destino
-     * @return TRUE se existe aresta, FALSE se não
-     */
     private boolean existeAresta(Vertice dest){
         boolean resposta = false;
         for(int i=0; i<this.qtArestas; i++){
-            if(this.arestas[i].getDestino() == dest){
+            if(this.arestas.get(i).getDestino() == dest){
                 resposta = true;
                 break;
             }
@@ -42,25 +34,16 @@ public class Vertice {
         return resposta;
 
     }
-    
-    /**
-     * Adiciona uma aresta neste vértice para um destino
-     * @param peso Peso da aresta (1 para grafos não ponderados)
-     * @param dest Vértice de destino
-     */
+
     public void addAresta(int peso, Vertice dest){
-        if(this.qtArestas < MAX_ARESTAS && !this.existeAresta(dest)){
+        if(!this.existeAresta(dest)){
             Aresta nova = new Aresta(peso, dest);
-            this.arestas[this.qtArestas] = nova;
+            this.arestas.add(nova);
             this.qtArestas++;
             dest.addAresta(peso, this);
         }
     }
 
-    /**
-     * Retorna o grau do vértice
-     * @return Grau (inteiro não negativo)
-     */
     public int grau(){
         return this.qtArestas;
     }
@@ -69,23 +52,22 @@ public class Vertice {
         ArrayList<Vertice> vertices = new ArrayList<>();
 
         for (int i = 0; i < qtArestas; i++) {
-            vertices.add(arestas[i].getDestino());
+            vertices.add(arestas.get(i).getDestino());
         }
 
         return vertices;
     }
 
-    
-    /**
-     * Retorna a descrição do vértice (id + vizinhos) em String
-     * @return Descrição do vértice
-     */
     @Override
     public String toString(){
-        StringBuilder aux = new StringBuilder("Vértice "+this.id+":");
+        StringBuilder aux = new StringBuilder("Vértice("+this.id+")=[");
+        for(int i = 0; i<this.qtArestas; i++) {
+            aux.append(this.arestas.get(i).getDestino().id);
+            if (i < this.qtArestas - 1)
+                aux.append(", ");
+        }
 
-        for(int i = 0; i<this.qtArestas; i++)
-            aux.append(this.arestas[i].getDestino().id+ " ");
+        aux.append("]");
 
         return aux.toString();
     }
