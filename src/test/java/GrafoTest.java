e7e2a4c646c94e0d055d60af08aec40f74253819
HugoPoletto34 <hugopoletto34@gmail.com>
2022-04-12 19:57:41 -0300

    Adicionando caso de teste de Grafo não-direcionado

--- src/test/java/GrafoTest.java
+++ src/test/java/GrafoTest.java
@@ -1,8 +1,21 @@
+import models.Grafo;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GrafoTest {
     @Test
-    public void test() {
-        System.out.println("Hello world");
+    public void tresVerticesConexosTest() {
+        Grafo grafo = new Grafo();
+        grafo.addVertice();
+        grafo.addVertice();
+        grafo.addVertice();
+
+        grafo.getVertice(0).addAresta(20, grafo.getVertice(1));
+        grafo.getVertice(1).addAresta(20, grafo.getVertice(2));
+        grafo.getVertice(2).addAresta(20, grafo.getVertice(0));
+
+        assertEquals("Grafo: --- Vértice(0)=[1, 2] --- Vértice(1)=[0, 2] --- Vértice(2)=[1, 0]", grafo.toString());
+
     }
 }
