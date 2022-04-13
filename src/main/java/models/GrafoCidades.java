0180936f95b4f9fa889646926aa1c5f28e6c4083
Pedro Aires <1212436@sga.pucminas.br>
2022-04-13 20:35:14 -0300

    teste distancia

--- src/main/java/models/GrafoCidades.java
+++ src/main/java/models/GrafoCidades.java
@@ -7,7 +7,7 @@ public class GrafoCidades extends Grafo {
     }
 
     // Método que lerá o arquivo de dados e criará todas as cidades.
-    public void gerarGrafoComCidades(){
+    public void gerarGrafoComCidades() {
         System.out.println("Gerando Grafo");
     }
 
@@ -15,7 +15,15 @@ public class GrafoCidades extends Grafo {
 
     }
 
-    public double calcularDistanciaCidades(Vertice v1, Vertice v2) {
-        return 0d;
+    public static double calcularDistanciaCidades(VerticeCidade v1, VerticeCidade v2) {
+        final double raioDaTerra = 6371;
+
+        return raioDaTerra
+                * Math.acos(
+                        Math.cos((Math.toRadians(90 - v1.getLatitude()))
+                                * Math.cos((Math.toRadians(90 - v2.getLatitude())) +
+                                        Math.sin(Math.toRadians(90 - v1.getLatitude()))
+                                                * Math.sin(90 - v2.getLatitude()))
+                                * Math.cos(Math.toRadians(v1.getLongitude() - v2.getLongitude()))));
     }
 }
--- src/test/java/GrafoCidadesTest.java
+++ src/test/java/GrafoCidadesTest.java
@@ -1,13 +1,23 @@
 
 import models.Grafo;
 import models.GrafoCidades;
+import models.VerticeCidade;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Ignore;
+
 public class GrafoCidadesTest {
     @Test
-    public void tresVerticesConexosTest() {
+    public void testeKMCidade() {
+        VerticeCidade v1, v2;
+        v1 = new VerticeCidade(1, "BH", -19.9281, -43.9419);
+        v2 = new VerticeCidade(2, "SP", -23.5504, -46.6339);
 
+        System.out.println(GrafoCidades.calcularDistanciaCidades(v1, v2));
+        // assertEquals(490.69, GrafoCidades.calcularDistanciaCidades(v1, v2));
     }
+
 }
